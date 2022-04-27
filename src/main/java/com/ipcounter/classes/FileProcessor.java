cfb6fd15dddc833c3538855a392d154ce6245819
MadPigeon <vsevolod007@gmail.com>
2022-04-27 15:19:47 +0600

    added class for processing files

---
+++ src/main/java/com/ipcounter/classes/FileProcessor.java
@@ -0,0 +1,21 @@
+package com.ipcounter.classes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileProcessor {
+
+  public static int countUniqueIpsInFile(final String filePath) throws IOException {
+    int result = 0;
+    try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+      result = Counter.countUniqueAddresses(lines);
+    } catch (IOException e) {
+      throw e;
+    }
+    
+    return result;
+  }
+
+}
---
+++ src/test/java/com/ipcounter/classes/FileProcessorTest.java
@@ -0,0 +1,31 @@
+package com.ipcounter.classes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class FileProcessorTest {
+
+  @Test
+  public void opensExistingFile() {
+    try {
+      int actualResult = FileProcessor.countUniqueIpsInFile("dataset/test.txt");
+      assertEquals(3, actualResult);
+    } catch (IOException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void tellsAboutMissingFIle() {
+    final String path = "dataset/no-such-file.txt";
+    try {
+      FileProcessor.countUniqueIpsInFile(path);
+      fail(String.format("Returns result without a file. Path(%s)", path));
+    } catch (IOException e) {
+    }
+  }
+}
