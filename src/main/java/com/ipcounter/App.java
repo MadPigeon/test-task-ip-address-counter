0a542dcda0d7117fd9976bdf2d82c487ee96da9f
MadPigeon <vsevolod007@gmail.com>
2022-04-27 15:53:38 +0600

    created a proper main to direct the user

--- src/main/java/com/ipcounter/App.java
+++ src/main/java/com/ipcounter/App.java
@@ -1,11 +1,24 @@
 package com.ipcounter;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.ipcounter.classes.FileProcessor;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please provide a path to the file with the list of IPs:");
+    String path = sc.nextLine();
+    try {
+      int uniqueAddressessCount = FileProcessor.countUniqueIpsInFile(path);
+      System.out.println(String.format("There are %s unique addressess in the file.", uniqueAddressessCount));
+    } catch (IOException e) {
+      System.out.println("An error has occured.");
+      System.out.println(e);
+      System.out.println(e.getMessage());
+    } finally {
+      sc.close();
+    }
   }
 }
