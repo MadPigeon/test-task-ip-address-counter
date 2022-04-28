67a7086aa06b6d0580102d190d0d255f6c7a0942
MadPigeon <vsevolod007@gmail.com>
2022-04-29 00:26:01 +0600

    added loop to main

--- src/main/java/com/ipcounter/App.java
+++ src/main/java/com/ipcounter/App.java
@@ -8,17 +8,22 @@ import com.ipcounter.classes.FileProcessor;
 public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Please provide a path to the file with the list of IPs:");
-    String path = sc.nextLine();
-    try {
-      int uniqueAddressessCount = FileProcessor.countUniqueIpsInFile(path);
-      System.out.println(String.format("There are %s unique addressess in the file.", uniqueAddressessCount));
-    } catch (IOException e) {
-      System.out.println("An error has occured.");
-      System.out.println(e);
-      System.out.println(e.getMessage());
-    } finally {
-      sc.close();
+    boolean finishedCounting = false;
+
+    while (!finishedCounting) {
+      System.out.println("Please provide a path to the file with the list of IPs:");
+      String path = sc.nextLine();
+      try {
+        long uniqueAddressessCount = FileProcessor.countUniqueIpsInFile(path);
+        System.out.println(String.format(
+            "There are %s unique addressess in the file.", uniqueAddressessCount));
+        finishedCounting = true;
+      } catch (IOException e) {
+        System.out.println("An error has occurred.");
+        System.out.println(e);
+        System.out.println(e.getMessage());
+      }
     }
+    sc.close();
   }
 }
